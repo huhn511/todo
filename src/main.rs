aca4fced41b97c3d2fb719aae84c3c04eb0fc994
huhn511 <sehe89@gmail.com>
2020-05-28 02:51:15 +0200

    :sparkles: add flexible server config
    user can configurate host and port in the .env variable

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
+mod config;
 mod models;
 
 use crate::models::Status;
 use actix_web::{HttpServer, App, web, Responder};
 use std::io;
+use dotenv::dotenv;
 
+// get status - /status
 async fn status() -> impl Responder {
     web::HttpResponse::Ok()
-        .json(Status {status: "Ok".to_string()})
+        .json(Status {status: "OK".to_string()})
 }
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    // Load the dotenv file
+    dotenv().ok();
+
+    // load config
+    let config = crate::config::Config::from_env().unwrap();
+
+    println!("Starting server at http://{}:{}", config.server.host, config.server.port);
+
+    // Start the server
     HttpServer::new( || {
         App::new()
-            .route("/", web::get().to(status))
+            .route("/status", web::get().to(status))
     })
-    .bind("0.0.0.0:3000")?
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
