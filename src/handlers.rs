8b08a141db951e5ecba8c0fb39a93a50ca3c42a2
huhn511 <sehe89@gmail.com>
2020-05-28 22:53:04 +0200

    :sparkles: add item enpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -18,3 +18,14 @@ pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
         Err(_) => HttpResponse::InternalServerError().into()
     }
 }
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+    let client: Client = 
+        db_pool.get().await.expect("Error connecting to the db");
+
+    let result = db::get_items(&client, path.0).await;
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into()
+    }
+}
